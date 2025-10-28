fa5d9d9eb277b5f2fe8967923f59e713473d53a3
Heijdenberg <jensheijdenberg@hotmail.com>
2025-10-28 14:53:55 +0100

    Add project files.

---
+++ iths-labb-3-quiz-configurator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace iths_labb_3_quiz_configurator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
