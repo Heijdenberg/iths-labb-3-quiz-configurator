a76382ed2a69b83fc5b36134cb774d5061395c72
Heijdenberg <jensheijdenberg@hotmail.com>
2025-11-12 17:50:12 +0100

    Set default values for properties in Result class
    Updated the `Question` and `Correct_answer` properties to have default values of empty strings (`""`). Also, set the `Incorrect_answers` property to default to an empty list (`new()`). This ensures the properties are initialized with sensible defaults, improving code reliability and reducing the risk of null reference issues.

--- iths-labb-3-quiz-configurator/Models/Result.cs
+++ iths-labb-3-quiz-configurator/Models/Result.cs
@@ -3,7 +3,7 @@ namespace iths_labb_3_quiz_configurator.Models;
 
 public class Result
 {
-    public string Question { get; set; }
-    public string Correct_answer { get; set; }
-    public List<string> Incorrect_answers { get; set; }
+    public string Question { get; set; } = "";
+    public string Correct_answer { get; set; } = "";
+    public List<string> Incorrect_answers { get; set; } = new();
 }
