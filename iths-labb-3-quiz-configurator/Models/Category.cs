c8371594cc6b1a2a84d03fab53778008474f23aa
Heijdenberg <jensheijdenberg@hotmail.com>
2025-11-12 17:47:02 +0100

    Set default value for `Name` property in `Category` class
    The `Name` property in the `Category` class was updated to include
    a default value of an empty string (`""`). This change ensures
    that the property is initialized by default, reducing the risk
    of null reference issues when the property is accessed before
    being explicitly set.

--- iths-labb-3-quiz-configurator/Models/Category.cs
+++ iths-labb-3-quiz-configurator/Models/Category.cs
@@ -4,5 +4,5 @@ namespace iths_labb_3_quiz_configurator.Models;
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
 }
